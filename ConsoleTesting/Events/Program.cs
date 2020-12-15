de71cea5533262fcdea74a7a584757d4adb80e90
Nenad Smontara <nsmontar@gmail.com>
2020-12-15 17:25:46 +0100

    Initial commit, working on MVC web app, already finished models, DAL, repository, web API

---
+++ ConsoleTesting/Events/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using static System.Console;
+
+namespace Events
+{
+    class Program
+    {
+        static int counter = 0;
+        static string displayString = 
+            "This string will appear one letter at a time. ";
+        static void Main(string[] args)
+        {
+            int num = counter;
+
+        }
+    }
+}
---
+++ WebAppNuGetPackages.cmd
@@ -0,0 +1,5 @@
+dotnet add SpyStore.Mvc package AutoMapper
+dotnet add SpyStore.Mvc package Newtonsoft.Json
+dotnet add SpyStore.Mvc package LigerShark.WebOptimizer.Core -v 1.0.236
+dotnet add SpyStore.Mvc package LigerShark.WebOptimizer.Sass -v 1.0.34-beta
+dotnet add SpyStore.Mvc package Microsoft.Web.LibraryManager.Build

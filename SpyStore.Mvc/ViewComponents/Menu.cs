98c47a8f69c2b2ebeffc3faf73c3d61a11d52fa3
Nenad Smontara <nsmontar@gmail.com>
2020-12-28 11:05:04 +0100

    Added 'Menu ViewComponent' and 'libman' library manager

---
+++ SpyStore.Mvc/ViewComponents/Menu.cs
@@ -0,0 +1,27 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using SpyStore.Mvc.Support;
+
+namespace SpyStore.Mvc.ViewComponents
+{
+    public class Menu : ViewComponent
+    {
+        private readonly SpyStoreServiceWrapper _serviceWrapper;
+        public Menu(SpyStoreServiceWrapper serviceWrapper)
+        {
+            _serviceWrapper = serviceWrapper;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var cats = await _serviceWrapper.GetCategoriesAsync();
+            if (cats == null)
+            {
+                return new ContentViewComponentResult("There was an error " + 
+                    "getting the categories");
+            }
+            return View("MenuView", cats);
+        }
+    }
+}
